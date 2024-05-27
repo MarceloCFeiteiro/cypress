6bf0f642fe1482636e099797bccd9d5804e33d89
MarceloCFeiteiro <36742565+MarceloCFeiteiro@users.noreply.github.com>
2024-05-27 14:17:52 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,19 +3,20 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-multi-reporters', // Necessário para a geração de reporters
   reporterOptions: {
-    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter', // Necessário para a geração de reporters
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter', // Necessário para a geração de reporters,
+        mochaJunitReporterReporterOptions: {// Configurações do reporter do junit
+      mochaFile: 'cypress/reports/junit/results-[hash].xml'
+    },
     cypressMochawesomeReporterReporterOptions: {// Configurações do reporter do mochawesome
       charts: true, // Informações gráficas
       reportPageTitle: 'Relatório de Testes',
       embeddedScreenshots: true, // Adiciona automáticamente os screenshots
       inlineAssets: true, // Informa que deve ser gerado apenas um arquivo html
       saveAllAttempts: false // Caso retentativas estejam configuradas, não deve registrar todas
-    },
-    mochaJunitReporterReporterOptions: {// Configurações do reporter do junit
-      mochaFile: 'cypress/reports/junit/results-[hash].xml'
     }
   },
   chromeWebSecurity: false,
+  videos: true,
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on) // Import necessário para reconhecimento das configs
